c7407f10848dad37b62600f4fb70e2b5684a4fcd
AlejandroHinojosaRubio <hinojosa.rub@gmail.com>
2025-03-04 16:19:55 +0100

    Chapter 1 & 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { CoffeesModule } from './coffees/coffees.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'pass123',
+      database: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       typePaths: ['./**/*.graphql'],
     }),
+    CoffeesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/coffees/entities/coffee.entity.ts
@@ -0,0 +1,27 @@
+import {
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import * as GraphQLTypes from '../../graphql-types';
+import { Flavor } from './flavor.entity';
+
+@Entity()
+export class Coffee implements GraphQLTypes.Coffee {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  brand: string;
+
+  @JoinTable()
+  @ManyToMany((type) => Flavor, (flavor) => flavor.coffees /* inverse side */, {
+    cascade: true,
+  })
+  flavors?: Flavor[];
+}
